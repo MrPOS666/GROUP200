452ca19a2eb2762202883165e33c7148c23907d8
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-08 20:02:50 -0500

    create entity

--- src/main/java/entity/CommonCocktail.java
+++ src/main/java/entity/CommonCocktail.java
@@ -1,4 +1,40 @@
 package entity;
 
-public class Cocktail {
+import java.util.List;
+
+/**
+ * The representation of a cocktail in our program.
+ */
+public class CommonCocktail implements Cocktail {
+    private final String name;
+    private final String recipe;
+    private final List<String> ingredients;
+    private final String photolink;
+
+    public CommonCocktail(String name, String recipe, List<String> ingredients, String photolink) {
+        this.name = name;
+        this.recipe = recipe;
+        this.ingredients = ingredients;
+        this.photolink = photolink;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getRecipe() {
+        return recipe;
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String getPhotoLink() {
+        return photolink;
+    }
 }
