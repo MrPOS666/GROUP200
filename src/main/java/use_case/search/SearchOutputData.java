ca7c828156edd83d37cd68bc9821e2d4fbe1171a
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-09 14:34:44 -0500

    search use case

--- src/main/java/use_case/search/SearchOutputData.java
+++ src/main/java/use_case/search/SearchOutputData.java
@@ -1,4 +1,19 @@
 package use_case.search;
 
+/**
+ * Output Data for the Search Use Case.
+ */
 public class SearchOutputData {
+
+    private final String cocktailName;
+    private final boolean useCaseFailed;
+
+    public SearchOutputData(String cocktailName, boolean useCaseFailed) {
+        this.cocktailName = cocktailName;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getCocktailName() {
+        return cocktailName;
+    }
 }
