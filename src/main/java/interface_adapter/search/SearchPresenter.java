2e9e53bd0f7a0ebeaa14dc000f469148c7803baa
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-09 18:12:36 -0500

    interface_adapter for search

--- src/main/java/interface_adapter/search/SearchPresenter.java
+++ src/main/java/interface_adapter/search/SearchPresenter.java
@@ -1,23 +1,44 @@
 package interface_adapter.search;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.result.ResultState;
+import interface_adapter.result.ResultViewModel;
 import use_case.search.SearchOutputBoundary;
 import use_case.search.SearchOutputData;
-import view.SearchView;
 
+/**
+ * The Presenter for the Search Use Case.
+ */
 public class SearchPresenter implements SearchOutputBoundary {
 
-    private SearchViewModel searchViewModel;
+    private final SearchViewModel searchViewModel;
+    private final ResultViewModel resultViewModel;
+    private final ViewManagerModel viewManagerModel;
 
-    public SearchPresenter(SearchViewModel searchViewModel) {
+    public SearchPresenter(SearchViewModel searchViewModel, ResultViewModel resultViewModel, ViewManagerModel viewManagerModel) {
         this.searchViewModel = searchViewModel;
+        this.resultViewModel = resultViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
-    public void prepareSuccessView(SearchOutputData searchOutputData) {
-        // On success, switch to the searched cocktail in view.
+    public void prepareSuccessView(SearchOutputData response) {
+        // On success, switch to the result in view.
 
-        final SearchState
-        searchViewModel.firePropertyChanged
+        final ResultState resultState = resultViewModel.getState();
+        ResultState.setCocktailName(response.getCocktailName());
+        this.resultViewModel.setState(resultState);
+        this.resultViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(ResultViewModel.giveViewName());
+        this.viewManagerModel.firePropertyChanged();
+
     }
 
+    @Override
+    public void prepareFailView(String errorMessage) {
+        final SearchState searchState = searchViewModel.getState();
+        searchState.setSearchError(errorMessage);
+        searchViewModel.firePropertyChanged();
+    }
 }
