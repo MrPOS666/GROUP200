9aee0ae4dcf41239a08a7149f7f8b6b83a3afe5d
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-11 01:55:15 -0500

    use case for back

--- src/main/java/use_case/back/BackInputBoundary.java
+++ src/main/java/use_case/back/BackInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.back;
 
+/**
+ * Input Boundary for actions which are related to switching back to the search page.
+ */
 public interface BackInputBoundary {
+
+    /**
+     * Executes the back use case.
+     * @param BackInputData the input data.
+     */
+    void execute(BackInputData BackInputData);
 }
