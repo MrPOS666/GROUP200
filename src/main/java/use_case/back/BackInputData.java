dcdbc9a370afcb643a7caeefd5f7c4e714958ba2
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-11 01:56:39 -0500

    use case for back

--- src/main/java/use_case/back/BackInputData.java
+++ src/main/java/use_case/back/BackInputData.java
@@ -1,4 +1,18 @@
 package use_case.back;
 
+/**
+ * The Input Data for the Back Use Case.
+ */
 public class BackInputData {
+
+    private final String cocktailName;
+
+
+    public BackInputData(String cocktailName) {
+        this.cocktailName = cocktailName;
+    }
+
+    String getCocktailName() {
+        return cocktailName;
+    }
 }
