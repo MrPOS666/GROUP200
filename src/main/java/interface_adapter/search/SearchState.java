b722356688ae400d2a437535cb027972d986cedd
YuxinLi88 <116744852+YuxinLi88@users.noreply.github.com>
2024-11-11 22:43:40 -0500

    search presenter

--- src/main/java/interface_adapter/search/SearchState.java
+++ src/main/java/interface_adapter/search/SearchState.java
@@ -1,9 +1,23 @@
 package interface_adapter.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchState {
     private String cocktailName = "";
+    private List<String> ingredients = new ArrayList<>();
     private String recipe = "";
     private String searchError;
+
+    public SearchState(SearchState previousState) {
+        this.cocktailName = previousState.cocktailName;
+        this.ingredients = previousState.ingredients;
+        this.recipe = previousState.recipe;
+        this.searchError = previousState.searchError;
+    }
+
+    public SearchState() {
+    }
 
     public String getCocktailName() {
         return cocktailName;
@@ -21,6 +35,11 @@ public class SearchState {
         this.searchError = searchError;
     }
 
+    public String getRecipe() {
+        return recipe;
+    }
 
-
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
 }
