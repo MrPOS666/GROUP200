77961f1e5ff0be701f9050ea491ec45de7c131f4
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-15 15:17:49 -0500

    entity

--- src/main/java/entity/CommonCocktailFactory.java
+++ src/main/java/entity/CommonCocktailFactory.java
@@ -1,4 +1,28 @@
 package entity;
 
-public class CommonCocktailFactory {
+import java.util.Map;
+
+/**
+ * Factory for creating CommonCocktail objects.
+ */
+public class CommonCocktailFactory implements CocktailFactory {
+
+    /**
+     * Create Cocktail.
+     *
+     * @param idDrink         id of cocktail
+     * @param strDrink        name of cocktail
+     * @param strInstructions instruction of cocktail
+     * @param photoUrl        photo URL of cocktail
+     * @param ingredients     ingredients of cocktail
+     * @return Cocktail
+     */
+    @Override
+    public Cocktail create(int idDrink,
+                           String strDrink,
+                           String strInstructions,
+                           String photoUrl,
+                           Map<String, String> ingredients) {
+        return new CommonCocktail(idDrink, strDrink, strInstructions, photoUrl, ingredients);
+    }
 }
