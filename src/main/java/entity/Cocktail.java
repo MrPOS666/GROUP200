3fd8b91c5e34dfd29e325308dcb7e4af5599da19
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-15 15:15:20 -0500

    entity

--- src/main/java/entity/Cocktail.java
+++ src/main/java/entity/Cocktail.java
@@ -1,6 +1,6 @@
 package entity;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * The representation of a cocktail in our program.
@@ -16,7 +16,7 @@ public interface Cocktail {
      * Returns the ingredient of the cocktail.
      * @return the ingredient of the cocktail.
      */
-    List<String> getIngredients();
+    Map<String, String> getIngredients();
 
     /**
      * Returns the photo link of the cocktail.
