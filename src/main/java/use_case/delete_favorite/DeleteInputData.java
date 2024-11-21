24bf155d16d2a31972c4ec4d18160ce772f4131a
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 15:25:47 -0500

    Use Case for delete

--- src/main/java/use_case/delete_favorite/DeleteInputData.java
+++ src/main/java/use_case/delete_favorite/DeleteInputData.java
@@ -1,4 +1,19 @@
 package use_case.delete_favorite;
 
+import java.util.List;
+
+/**
+ * The Input Data for the Delete Use Case.
+ */
 public class DeleteInputData {
+
+    private final List<Integer> deleteCocktailNames;
+
+    public DeleteInputData(List<Integer> deleteCocktailNames) {
+        this.deleteCocktailNames = deleteCocktailNames;
+    }
+
+    public List<Integer> getDeleteCocktailNames() {
+        return deleteCocktailNames;
+    }
 }
