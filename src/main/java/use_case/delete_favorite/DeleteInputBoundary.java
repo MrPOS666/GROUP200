293b8de2adc56706889c2c5efaba9dc4762533fd
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 15:25:19 -0500

    Use Case for delete

--- src/main/java/use_case/delete_favorite/DeleteInputBoundary.java
+++ src/main/java/use_case/delete_favorite/DeleteInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.delete_favorite;
 
+/**
+ * Input Boundary for actions which are related to deleting.
+ */
 public interface DeleteInputBoundary {
+
+    /**
+     * Executes the delete use case.
+     * @param deleteInputData the input data
+     */
+    void execute(DeleteInputData deleteInputData);
 }
