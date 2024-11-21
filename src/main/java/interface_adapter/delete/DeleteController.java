b017bae29ee73790b91f5f3bd669150ebb4fc5b8
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 14:57:02 -0500

    add delete interface_adapter

--- src/main/java/interface_adapter/delete/DeleteController.java
+++ src/main/java/interface_adapter/delete/DeleteController.java
@@ -1,4 +1,27 @@
-package interface_adapter.myFavourite;
+package interface_adapter.delete;
 
+import java.util.List;
+
+import use_case.delete_favorite.DeleteInputBoundary;
+import use_case.delete_favorite.DeleteInputData;
+
+/**
+ * Controller for the Delete Use Case.
+ */
 public class DeleteController {
+    private final DeleteInputBoundary deleteInteractor;
+
+    public DeleteController(DeleteInputBoundary deleteInteractor) {
+        this.deleteInteractor = deleteInteractor;
+    }
+
+    /**
+     * Executes the Delete Use Case.
+     * @param selectCocktail the selected cocktail ID.
+     */
+    public void execute(List<Integer> selectCocktail) {
+        final DeleteInputData deleteInputData = new DeleteInputData(selectCocktail);
+
+        deleteInteractor.execute(deleteInputData);
+    }
 }
--- src/main/java/interface_adapter/delete/DeleteState.java
+++
@@ -1,4 +0,0 @@
-package interface_adapter.delete;
-
-public class DeleteState {
-}
