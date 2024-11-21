7971fb31f7266e85b28255890d606a3546dd994a
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 15:09:57 -0500

    select interface_adapter

--- src/main/java/interface_adapter/select/SelectState.java
+++ src/main/java/interface_adapter/select/SelectState.java
@@ -1,4 +1,21 @@
 package interface_adapter.select;
 
+import java.util.List;
+
+/**
+ * State for select.
+ */
 public class SelectState {
+
+    public String getCocktail() {
+    }
+
+    public List<Integer> getSelectedCocktails() {
+    }
+
+    public void setSelectedCocktails(List<Integer> selectedCocktails) {
+    }
+
+    public List<Integer> getCocktailList() {
+    }
 }
