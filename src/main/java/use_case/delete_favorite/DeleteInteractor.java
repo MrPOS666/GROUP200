8bffa22810c2757ea8d74e3c3468bbc8fe2e33d8
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 15:27:20 -0500

    Use Case for delete

--- src/main/java/use_case/delete_favorite/DeleteInteractor.java
+++ src/main/java/use_case/delete_favorite/DeleteInteractor.java
@@ -1,4 +1,25 @@
 package use_case.delete_favorite;
 
-public class DeleteInteractor {
+/**
+ * The Delete Interactor.
+ */
+public class DeleteInteractor implements DeleteInputBoundary {
+    private final DeleteDataAccessInterface deleteDataAccessObject;
+    private final DeleteOutputBoundary deletePresenter;
+
+    public DeleteInteractor(DeleteDataAccessInterface deleteDataAccessObject, DeleteOutputBoundary deletePresenter) {
+        this.deleteDataAccessObject = deleteDataAccessObject;
+        this.deletePresenter = deletePresenter;
+    }
+
+    /**
+     * Executes the delete use case.
+     *
+     * @param deleteInputData the input data
+     */
+    @Override
+    public void execute(DeleteInputData deleteInputData) {
+
+        if (deleteInputData)
+    }
 }
