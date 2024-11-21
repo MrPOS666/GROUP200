dd517b89e0312a83c9b8ec00a5743cfc25c24d63
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 14:57:48 -0500

    app main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,15 +1,6 @@
 package app;
 
-import interface_adapter.search.SearchController;
-import interface_adapter.search.SearchViewModel;
-import use_case.search.SearchDataAccessInterface;
-import use_case.search.SearchInputBoundary;
-import use_case.search.SearchInteractor;
-import use_case.search.SearchOutputBoundary;
-import view.SearchView;
-
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
 
 /**
  * The Main class of our application.
@@ -20,26 +11,7 @@ public class Main {
      * @param args unused arguments
      */
     public static void main(String[] args) {
-        //TODO
-        //final SearchInputBoundary searchInteractor = new SearchInteractor(searchDataAccessInterface,
-               //searchOutputBoundary);
-
-        //SearchController searchController = new SearchController(searchInteractor);
-
-        SearchViewModel searchViewModel = new SearchViewModel("search");
-        SearchView searchView = new SearchView(searchViewModel);
-        //searchView.setSearchController(searchController);
-        JPanel panel = new JPanel();
-        panel.add(searchView, searchView.getViewName());
-
-        Frame frame = new JFrame("Simple Swing App");
-        frame.setSize(600, 300);
-        ((JFrame) frame).setContentPane(panel);
-        ((JFrame) frame).setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
         final AppBuilder appBuilder = new AppBuilder();
-
         final JFrame application = appBuilder
                                             .addLoginView()
                                             .addSignupView()
@@ -51,7 +23,7 @@ public class Main {
                                             .addLogoutUseCase()
                                             .build();
 
-        //application.pack();
-        //application.setVisible(true);
+        application.pack();
+        application.setVisible(true);
     }
 }
