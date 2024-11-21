1ab001c8eeb2cade4d5854afedd9cecd2d6bdfec
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 14:58:15 -0500

    add delete interface_adapter

--- src/main/java/interface_adapter/delete/DeletePresenter.java
+++ src/main/java/interface_adapter/delete/DeletePresenter.java
@@ -1,4 +1,30 @@
-package interface_adapter.myFavourite;
+package interface_adapter.delete;
 
-public class DeletePresenter {
+import interface_adapter.ViewModel;
+import interface_adapter.select.SelectState;
+import interface_adapter.select.SelectViewModel;
+import use_case.delete_favorite.DeleteOutputBoundary;
+import use_case.search.SearchOutputData;
+
+import javax.swing.text.View;
+
+/**
+ * The Presenter for the Delete Use Case.
+ */
+public class DeletePresenter implements DeleteOutputBoundary {
+
+    private final SelectViewModel selectViewModel;
+    private final ViewModel viewManagerModel;
+
+    public DeletePresenter(SelectViewModel selectViewModel, ViewModel viewManagerModel) {
+        this.selectViewModel = selectViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SearchOutputData response) {
+        final SelectState selectState = selectViewModel.getState();
+
+
+    }
 }
