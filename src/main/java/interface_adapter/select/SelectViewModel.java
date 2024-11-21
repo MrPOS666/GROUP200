6dc4dc2393a8074c810ae082912ab6e49a550e94
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-21 15:09:33 -0500

    select interface_adapter

--- src/main/java/interface_adapter/select/SelectViewModel.java
+++ src/main/java/interface_adapter/select/SelectViewModel.java
@@ -1,4 +1,15 @@
 package interface_adapter.select;
 
+import view.MyFavouriteView;
+
 public class SelectViewModel {
+    public void addPropertyChangeListner(MyFavouriteView myFavouriteView) {
+    }
+
+    public SelectState getState() {
+    }
+
+    public void setState(SelectState currentState) {
+
+    }
 }
