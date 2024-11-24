9d8984179105b77acdf330707a2648c94c1cd7c4
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-24 16:02:40 -0500

    interface adapter for delete

--- src/main/java/interface_adapter/select/SelectState.java
+++ src/main/java/interface_adapter/select/SelectState.java
@@ -7,21 +7,62 @@ import java.util.List;
  */
 public class SelectState {
 
-    public String getCocktail() {
+    private String username; // The username of the current user
+    private List<Integer> selectedCocktails; // List of selected cocktail IDs
+    private List<Integer> cocktailList; // List of all available cocktail IDs
+    private String selectError; // Error message, if any
+
+    /**
+     * Gets the username of the current user.
+     *
+     * @return the username.
+     */
+    public String getUsername() {
+        return username;
     }
 
+    /**
+     * Sets the username of the current user.
+     *
+     * @param username the username to set.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Gets the list of selected cocktail IDs.
+     *
+     * @return the list of selected cocktail IDs.
+     */
     public List<Integer> getSelectedCocktails() {
+        return selectedCocktails;
     }
 
+    /**
+     * Sets the list of selected cocktail IDs.
+     *
+     * @param selectedCocktails the list of selected cocktail IDs.
+     */
     public void setSelectedCocktails(List<Integer> selectedCocktails) {
+        this.selectedCocktails = selectedCocktails;
     }
 
+    /**
+     * Gets the list of all available cocktail IDs.
+     *
+     * @return the list of all cocktail IDs.
+     */
     public List<Integer> getCocktailList() {
+        return cocktailList;
     }
 
-    public void firePropertyChanged() {
-    }
-
+    /**
+     * Gets the error message for selection.
+     *
+     * @param errorMessage the error message.
+     */
     public void setSelectError(String errorMessage) {
+        this.selectError = errorMessage;
     }
 }
