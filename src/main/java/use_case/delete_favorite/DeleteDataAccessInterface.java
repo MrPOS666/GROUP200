65dc22e024276003685f6053fc58f84eb2eef589
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-24 16:03:12 -0500

    delete use case

--- src/main/java/use_case/delete_favorite/DeleteDataAccessInterface.java
+++ src/main/java/use_case/delete_favorite/DeleteDataAccessInterface.java
@@ -1,4 +1,22 @@
 package use_case.delete_favorite;
 
+import entity.User;
+
+/**
+ * DAO for the Delete Use Case.
+ */
 public interface DeleteDataAccessInterface {
+
+    /**
+     * Returns the user by its name.
+     * @param username the username
+     * @return user
+     */
+    User getUserByUsername(String username);
+
+    /**
+     * Updates the user favorite cocktail list.
+     * @param user user
+     */
+    void updateFavorite(User user);
 }
