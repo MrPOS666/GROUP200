263292cadb3cdf3f1cd0a64139975c035ae04e28
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-24 16:03:57 -0500

    delete use case

--- src/main/java/use_case/delete_favorite/DeleteOutputData.java
+++ src/main/java/use_case/delete_favorite/DeleteOutputData.java
@@ -1,4 +1,53 @@
 package use_case.delete_favorite;
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Output Data for the Delete Use Case.
+ */
 public class DeleteOutputData {
+
+    private final boolean useCaseFailed;
+
+    private final List<Integer> idDrink;
+    private final List<String> strDrink;
+    private final List<String> strInstructions;
+    private final List<String> photoUrl;
+    // Ingredient name as key, measure as value
+    private final List<Map<String, String>> ingredients;
+
+    public DeleteOutputData(boolean useCaseFailed, List<Integer> idDrink, List<String> strDrink, List<String> strInstructions, List<String> photoUrl, List<Map<String, String>> ingredients) {
+        this.useCaseFailed = useCaseFailed;
+        this.idDrink = idDrink;
+        this.strDrink = strDrink;
+        this.strInstructions = strInstructions;
+        this.photoUrl = photoUrl;
+        this.ingredients = ingredients;
+    }
+
+    // Getters for accessing fields
+    public List<Integer> getIdDrink() {
+        return idDrink;
+    }
+
+    public List<String> getCocktailName() {
+        return strDrink;
+    }
+
+    public List<String> getInstructions() {
+        return strInstructions;
+    }
+
+    public List<String> getPhotoLink() {
+        return photoUrl;
+    }
+
+    public List<Map<String, String>> getIngredients() {
+        return ingredients;
+    }
+
+    public boolean isUseCaseFailed() {
+        return useCaseFailed;
+    }
 }
