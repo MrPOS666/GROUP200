22b2cc83c65e4a19f97888d0c7df2e5cb0d880f1
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-26 02:18:37 -0500

    add missing file

--- src/main/java/interface_adapter/recommendation/RecommendationViewModel.java
+++ src/main/java/interface_adapter/recommendation/RecommendationViewModel.java
@@ -1,4 +1,15 @@
 package interface_adapter.recommendation;
 
-public class RecommendationViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Login View.
+ */
+public class RecommendationViewModel extends ViewModel<RecommendationState> {
+
+    public RecommendationViewModel() {
+        super("Recommendation");
+        setState(new RecommendationState());
+    }
+
 }
