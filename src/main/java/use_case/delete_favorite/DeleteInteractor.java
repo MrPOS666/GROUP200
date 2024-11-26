f9ad6c70e3a71ba7de6560923e392f0589214eb4
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-26 01:37:39 -0500

    change to use loadCocktails, savaCocktails, getUserByUsername

--- src/main/java/use_case/delete_favorite/DeleteInteractor.java
+++ src/main/java/use_case/delete_favorite/DeleteInteractor.java
@@ -6,16 +6,17 @@ import java.util.Map;
 
 import entity.Cocktail;
 import entity.User;
+import use_case.detailPage.DetailPageDataAccessException;
 
 /**
  * The Delete Interactor.
  */
 public class DeleteInteractor implements DeleteInputBoundary {
-    private final DeleteDataAccessInterface deleteDataAccessObject;
+    private final DeleteDataAccessInterface dbUserDataAccessObject2;
     private final DeleteOutputBoundary deletePresenter;
 
     public DeleteInteractor(DeleteDataAccessInterface deleteDataAccessObject, DeleteOutputBoundary deletePresenter) {
-        this.deleteDataAccessObject = deleteDataAccessObject;
+        this.dbUserDataAccessObject2 = deleteDataAccessObject;
         this.deletePresenter = deletePresenter;
     }
 
@@ -25,16 +26,23 @@ public class DeleteInteractor implements DeleteInputBoundary {
      * @param deleteInputData the input data
      */
     @Override
-    public void execute(DeleteInputData deleteInputData) {
+    public void execute(DeleteInputData deleteInputData) throws DetailPageDataAccessException {
         final String username = deleteInputData.getUserName();
         final List<Integer> cocktailIdToDelete = deleteInputData.getDeleteCocktailId();
+        final User user = dbUserDataAccessObject2.getUserByUsername(username);
 
-        final User user = deleteDataAccessObject.getUserByUsername(username);
-        if (user == null) {
-            deletePresenter.prepareFailView("User not found: " + username);
+        final List<Integer> ids = new ArrayList<>();
+        final List<String> names = new ArrayList<>();
+        final List<String> instructions = new ArrayList<>();
+        final List<String> photoLinks = new ArrayList<>();
+        final List<Map<String, String>> ingredients = new ArrayList<>();
+
+        // Fetch the list of favorite cocktails directly using the username
+        final List<Cocktail> favourites = dbUserDataAccessObject2.loadCocktails(user);
+        if (favourites == null || favourites.isEmpty()) {
+            deletePresenter.prepareFailView("No favorites found for user: " + username);
         }
 
-        final List<Cocktail> favourites = user.getMyFavourite();
         final List<Cocktail> updateFavourites = new ArrayList<>(favourites);
         boolean deleted = false;
 
@@ -46,10 +54,17 @@ public class DeleteInteractor implements DeleteInputBoundary {
         }
 
         if (deleted) {
+            for (Cocktail cocktail : updateFavourites) {
+                ids.add(cocktail.getIdDrink());
+                names.add(cocktail.getCocktailName());
+                instructions.add(cocktail.getInstructions());
+                photoLinks.add(cocktail.getPhotoLink());
+                ingredients.add(cocktail.getIngredients());
+            }
             user.getMyFavourite().clear();
             user.getMyFavourite().addAll(updateFavourites);
             final DeleteOutputData outputData = createOutputData(updateFavourites);
-            deleteDataAccessObject.updateFavorite(user, outputData.getIdDrink());
+            dbUserDataAccessObject2.saveCocktails(user, updateFavourites);
 
             deletePresenter.prepareSuccessView(outputData);
         }
