da9a677e1fe279cbf6837edaa72ccf2e7091e6f8
stanway.cen <stanway.cen@mail.utoronto.ca>
2024-11-28 14:57:39 -0500

    temp

--- src/main/java/interface_adapter/recommendation/RecommendationViewModel.java
+++ src/main/java/interface_adapter/recommendation/RecommendationViewModel.java
@@ -1,9 +1,13 @@
 package interface_adapter.recommendation;
 
-public class RecommendationViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.signup.SignupState;
+
+public class RecommendationViewModel extends ViewModel<RecommendationState> {
 
     public String[] getRecommendations() {
         // Simulate fetching recommendations (replace with real logic later)
         return new String[] {"Mojito", "Martini", "Margarita", "Pina Colada", "Bloody Mary", "Whiskey Sour"};
     }
 }
+
