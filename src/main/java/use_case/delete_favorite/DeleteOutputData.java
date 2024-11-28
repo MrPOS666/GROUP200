cf2166db39af756027af0bdebdfd0867fdd08cee
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-27 19:34:01 -0500

    new output boundary  according to the modified DAO

--- src/main/java/use_case/delete_favorite/DeleteOutputData.java
+++ src/main/java/use_case/delete_favorite/DeleteOutputData.java
@@ -1,5 +1,6 @@
 package use_case.delete_favorite;
 
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.Map;
 
@@ -16,14 +17,16 @@ public class DeleteOutputData {
     private final List<String> photoUrl;
     // Ingredient name as key, measure as value
     private final List<Map<String, String>> ingredients;
+    private final List<BufferedImage> images;
 
-    public DeleteOutputData(boolean useCaseFailed, List<Integer> idDrink, List<String> strDrink, List<String> strInstructions, List<String> photoUrl, List<Map<String, String>> ingredients) {
+    public DeleteOutputData(boolean useCaseFailed, List<Integer> idDrink, List<String> strDrink, List<String> strInstructions, List<String> photoUrl, List<Map<String, String>> ingredients, List<BufferedImage> images) {
         this.useCaseFailed = useCaseFailed;
         this.idDrink = idDrink;
         this.strDrink = strDrink;
         this.strInstructions = strInstructions;
         this.photoUrl = photoUrl;
         this.ingredients = ingredients;
+        this.images = images;
     }
 
     // Getters for accessing fields
@@ -45,6 +48,10 @@ public class DeleteOutputData {
 
     public List<Map<String, String>> getIngredients() {
         return ingredients;
+    }
+
+    public List<BufferedImage> getImages() {
+        return images;
     }
 
     public boolean isUseCaseFailed() {
