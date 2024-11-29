0e383e9f432c2f5f40e625eee89898b5f3509eae
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-28 20:34:37 -0500

    implement the functions from the delete interface

--- src/main/java/data_access/DBUserDataAccessObject2.java
+++ src/main/java/data_access/DBUserDataAccessObject2.java
@@ -221,6 +221,34 @@ public class DBUserDataAccessObject2 implements DeleteDataAccessInterface {
         }
     }
 
+    /**
+     * Update the MyFavourite cocktail list for an existing user.
+     *
+     * @param user          The user object containing the username and password.
+     * @param newFavourites The new list of favorite cocktails.
+     * @throws DetailPageDataAccessException If an error occurs during the process.
+     */
+    @Override
+    public void updateMyFavouriteCocktail(User user, List<Cocktail> newFavourites) throws DetailPageDataAccessException {
+        updateMyFavourite(user, newFavourites);
+    }
+
+    /**
+     * Load a user by their username.
+     *
+     * @param username The username of the user.
+     * @return The User object.
+     * @throws DetailPageDataAccessException If an error occurs during the process.
+     */
+    @Override
+    public User loadUserByName(String username) throws DetailPageDataAccessException {
+        return loadUser(username);
+    }
+
+    @Override
+    public void saveUserToApi(User testUser) throws DetailPageDataAccessException {
+        saveUser(testUser);
+    }
 }
 
 
--- src/main/java/use_case/detailPage/DetailPageDataAccessInterface.java
+++
@@ -1,4 +0,0 @@
-package use_case.detailPage;
-
-public interface DetailPageDataAccessInterface {
-}
