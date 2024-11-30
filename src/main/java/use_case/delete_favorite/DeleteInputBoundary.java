0172ca3f2367d5eb15afc8ffd071b803ec7d5f86
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-30 17:22:37 -0500

    add myFavouritePageDataAccessInterface

--- src/main/java/use_case/delete_favorite/DeleteInputBoundary.java
+++ src/main/java/use_case/delete_favorite/DeleteInputBoundary.java
@@ -1,6 +1,6 @@
 package use_case.delete_favorite;
 
-import use_case.detailPage.DetailPageDataAccessException;
+import use_case.detailPage.MyfavouritePageDataAccessException;
 
 /**
  * Input Boundary for actions which are related to deleting.
@@ -10,9 +10,9 @@ public interface DeleteInputBoundary {
     /**
      * Executes the delete use case.
      * @param deleteInputData the input data
-     * @throws DetailPageDataAccessException the exception
+     * @throws MyfavouritePageDataAccessException the exception
      */
-    void execute(DeleteInputData deleteInputData) throws DetailPageDataAccessException;
+    void execute(DeleteInputData deleteInputData) throws MyfavouritePageDataAccessException;
 
     /**
      * Execute the switch to Homepage View use case.
