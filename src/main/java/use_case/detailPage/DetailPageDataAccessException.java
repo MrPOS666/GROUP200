5fbe2011482590dd2fe87f6f000de65bfd969b97
Qianwen Wang <vivianqianwen.wang@mail.utoronto.ca>
2024-11-30 18:55:08 -0500

    delete and redo the detailpagedataaccess Exception

---
+++ src/main/java/use_case/detailPage/DetailPageDataAccessException.java
@@ -0,0 +1,7 @@
+package use_case.detailPage;
+
+public class DetailPageDataAccessException extends RuntimeException {
+    public DetailPageDataAccessException(String message) {
+        super(message);
+    }
+}
